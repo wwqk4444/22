303d6de682809bd9cd7ca02b1c60c0f98d310b1a
wwqk4444 <wwqk4444@163.com>
2023-11-07 21:09:46 +0800

    1107

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# 22
+## kcpraw
+
+运行在伪造的 TCP 协议之上的 kcptun, 主要目的是避免 ISP 对 UDP 协议可能的 QOS.  
+在三次握手后会进行 HTTP 握手, 将流量伪装成 HTTP 流量.  
+kcptun 的具体参数与使用方法参见 [kcptun](https://github.com/xtaci/kcptun)  
+
+### 基本用法  
+
+为了使用原始套接字，服务端与客户端都需要 root 权限  
+  
+服务端  
+```
+./kcpraw_server_linux_amd64 -t "TARGET_IP:TARGET_PORT" -l "KCP_SERVER_IP:KCP_SERVER_PORT"
+```  
+客户端  
+```
+./kcpraw_client_darwin_amd64 -r "KCP_SERVER_IP:KCP_SERVER_PORT" -l ":LOCAL_PORT"
+```
+
+### 注意事项
+~~为了避免内核返回的 RST 报文影响连接的建立，需要添加相应的 iptables 规则~~  
+现在 linux 下的客户端和服务端会自动添加和清理 iptables 规则  
+
+windows 客户端参考 [windows firewall port exceptions](https://www.veritas.com/support/en_US/article.000085856) 链接中的方法为 LOCAL_PORT 设置防火墙规则  
+windows 下推荐直接下载编译好的客户端,依赖 [winpcap](http://www.winpcap.org/install/)  
+
+~~macos 下使用客户端可以参考 [enable steath mode](http://osxdaily.com/2015/11/18/enable-stealth-mode-mac-os-x-firewall/) 打开静默模式即可~~  
+macos 下使用推荐通过设置 pf 规则来过滤 RST 报文 
+```
+# 在 /etc/pf.conf 文件后添加一行 
+block drop proto tcp from any to any flags R/R
+# 然后在终端下执行 
+sudo pfctl -f /etc/pf.conf 
+sudo pfctl -e 
+```
+
+如果找不到方法过滤 RST 报文,现在可以使用选项 --ignrst 来在应用层忽略 RST 报文,但是并不推荐使用这个选项,因为并不能保证 RST 发出去以后连接还能继续保持,请谨慎使用  
+
+由于使用 windows 或者 macos 服务器运行 kcptun 服务的情况并不多见, 并没有进行相应的测试, 所以并不能保证服务端在非 linux 环境下能够正常使用  
+
+伪装的 Host 可以通过选项 --host <name> 进行设置  
+如果不希望伪装为 HTTP 流量可以通过设置选项 --nohttp 关闭此功能, 注意客户端和服务端在这一选项上必须保持一致
+
+### 构建
+
+```
+go get -u -v github.com/ccsexyz/kcpraw/client  
+go get -u -v github.com/ccsexyz/kcpraw/server  
+```
+
+windows 下编译依赖 [winpcap](http://www.winpcap.org/install/) 和 gcc 请自行解决环境问题    
+对 windows10 使用者你可能需要参考这个链接 [stackoverflow](http://stackoverflow.com/questions/38047858/compile-gopacket-on-windows-64bit)　　
---
+++ build-release.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+MD5='md5sum'
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+	MD5='md5'
+fi
+
+UPX=false
+if hash upx 2>/dev/null; then
+	UPX=true
+fi
+
+VERSION=`date -u +%Y%m%d`
+LDFLAGS="-X main.VERSION=$VERSION -s -w"
+GCFLAGS=""
+
+OSES=(linux darwin)
+ARCHS=(amd64 386)
+for os in ${OSES[@]}; do
+	for arch in ${ARCHS[@]}; do
+		suffix=""
+		if [ "$os" == "windows" ]
+		then
+			suffix=".exe"
+		fi
+        cgo_enabled=1
+        if [ "$os" == "linux" ]
+        then 
+            cgo_enabled=0
+        fi 
+        env CGO_ENABLED=$cgo_enabled GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_client_${os}_${arch}${suffix} github.com/ccsexyz/kcpraw/client
+        env CGO_ENABLED=$cgo_enabled GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_server_${os}_${arch}${suffix} github.com/ccsexyz/kcpraw/server
+		if $UPX; then upx -9 kcpraw_client_${os}_${arch}${suffix} kcpraw_server_${os}_${arch}${suffix};fi
+		tar -zcf kcpraw-${os}-${arch}-$VERSION.tar.gz kcpraw_client_${os}_${arch}${suffix} kcpraw_server_${os}_${arch}${suffix}
+		$MD5 kcpraw-${os}-${arch}-$VERSION.tar.gz
+	done
+done
+
+# ARM
+ARMS=(5 6 7)
+for v in ${ARMS[@]}; do
+	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_client_linux_arm$v  github.com/ccsexyz/kcpraw/client
+	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_server_linux_arm$v  github.com/ccsexyz/kcpraw/server
+done
+if $UPX; then upx -9 kcpraw_client_linux_arm* kcpraw_server_linux_arm*;fi
+tar -zcf kcpraw-linux-arm-$VERSION.tar.gz kcpraw_client_linux_arm* kcpraw_server_linux_arm*
+$MD5 kcpraw-linux-arm-$VERSION.tar.gz
+
+#MIPS32LE
+env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_client_linux_mipsle github.com/ccsexyz/kcpraw/client
+env CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_server_linux_mipsle github.com/ccsexyz/kcpraw/server
+env CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_client_linux_mips github.com/ccsexyz/kcpraw/client
+env CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o kcpraw_server_linux_mips github.com/ccsexyz/kcpraw/server
+
+if $UPX; then upx -9 kcpraw_client_linux_mips* kcpraw_server_linux_mips*;fi
+tar -zcf kcpraw-linux-mipsle-$VERSION.tar.gz kcpraw_client_linux_mipsle kcpraw_server_linux_mipsle
+tar -zcf kcpraw-linux-mips-$VERSION.tar.gz kcpraw_client_linux_mips kcpraw_server_linux_mips
+$MD5 kcpraw-linux-mipsle-$VERSION.tar.gz
+$MD5 kcpraw-linux-mips-$VERSION.tar.gz
