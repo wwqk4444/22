2c76c44157f407bb6b600aa6ee41ac10d06137ed
wwqk4444 <wwqk4444@163.com>
2023-11-13 15:03:33 +0800

    Update main.go

--- client/main.go
+++ client/main.go
@@ -22,7 +22,7 @@ import (
 	"github.com/wwqk4444/kcp-go-raw"
 	"github.com/wwqk4444/s-go/redir"
 	"github.com/wwqk4444/s-go/shadowsocks"
-	"github.com/wwqk4444/smux"
+	"github.com/wwqk4444/smux/@latest"
 	"github.com/wwqk4444/ccsexyz_utils"
 	"github.com/golang/snappy"
 	"github.com/pkg/errors"
