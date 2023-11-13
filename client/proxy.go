a55e97c4d6f551fd72858c545681fc48a82226e1
wwqk4444 <wwqk4444@163.com>
2023-11-13 14:18:31 +0800

    14:18

--- client/proxy.go
+++ client/proxy.go
@@ -9,8 +9,8 @@ import (
 	"os"
 	"sync"
 
-	"github.com/ccsexyz/shadowsocks-go/shadowsocks"
-	"github.com/ccsexyz/utils"
+	"github.com/wwqk4444/ccsexyz_utils"
+	"github.com/wwqk4444/s-go/shadowsocks"
 )
 
 func socks6HandShake(conn net.Conn, host string, port int) error {
