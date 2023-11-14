55f01c258c33732cf95d61e892f3cbdd4757bad9
wwqk4444 <wwqk4444@163.com>
2023-11-14 19:27:34 +0800

    Update main.go

--- client/main.go
+++ client/main.go
@@ -19,7 +19,7 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/golang/snappy"
+	"github.com/wwqk4444/snappy"
 	"github.com/pkg/errors"
 	"github.com/urfave/cli"
 	"github.com/wwqk4444/kcp-go-raw"
